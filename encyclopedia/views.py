9e7c8246579f350bad5fe1652e8dd3d2097f2a82
Lucas Jacinto <lucasjacintodosantos@gmail.com>
2023-06-02 15:11:34 -0300

    implementing entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-
+from django.http  import  HttpResponseRedirect
+from django.urls import reverse
 from . import util
+from django import forms
 
 
 def index(request):
@@ -8,3 +10,19 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request):
+    markdown = Markdown()
+    entryPages = util.get_entry(entry)
+    if entryPages is None:
+        return render(request, "encyclopedia/noExistEntry.html",{
+            "entryTitle":entry
+        })
+
+def search(request):
+    value = request.GET.get("q",'')
+    if(util.get_entry(value) is not None):
+        return HttpResponseRedirect(reverse("entry", kwargs={'entry': value }))
+    else:
+        StringEntries = []
+        for entry in util.list_entries():
+            StringEntries.append(entry)
