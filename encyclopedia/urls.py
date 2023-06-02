eb3aeb2211541c217ffe69ab9812d4bf38261840
Lucas Jacinto <lucasjacintodosantos@gmail.com>
2023-06-02 15:36:18 -0300

    adding paths

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,5 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("wiki/<str:entry>", views.entry, name="entry"),
+    path("search", views.search, name="search"),
+    path("random", views.random, name="random"),
+     path("wiki/newEntry", views.newEntry, name="newEntry"),
+      path("wiki/<str:entry>/edit", views.edit, name="edit")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http  import  HttpResponseRedirect
 from django.urls import reverse
 from . import util
+from markdown2 import Markdown
 from django import forms
 
 
@@ -10,12 +11,17 @@ def index(request):
         "entries": util.list_entries()
     })
 
-def entry(request):
+def entry(request, entry):
     markdown = Markdown()
     entryPages = util.get_entry(entry)
     if entryPages is None:
         return render(request, "encyclopedia/noExistEntry.html",{
             "entryTitle":entry
+        })
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "entry":markdown.convert(entryPages),
+            "entryTitle": entry
         })
 
 def search(request):
