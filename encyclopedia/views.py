33baabe6373b61a0852ee64202fe0f65cc8fb3ca
Lucas Jacinto <lucasjacintodosantos@gmail.com>
2023-06-02 16:45:16 -0300

    implementing newEntry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -45,3 +45,35 @@ def random(request):
     entries = util.list_entries()
     randomEntries = secrets.choice(entries)
     return HttpResponseRedirect(reverse("entry", kwargs={'entry':randomEntries}))
+
+
+class newEntryForm(forms.Form):
+    title = forms.CharField(label = "Entry the Title", widget=forms.TextInput(attrs={'class' : 'form-control col-md-8 col-lg-8'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control col-md-8 col-lg-8', 'rows': 10}))
+    edit = forms.BooleanField(initial=False, widget=forms.HiddenInput(), required=False)
+
+def newEntry(request):
+    if request.method == "POST":
+        form = newEntryForm(request.POST)
+        if form.is_valid():
+            title = form.cleaned_data["title"]
+            content = form.cleaned_data["content"]
+            if(util.get_entry(title) is None or form.cleaned_data["edit"] is True):
+                util.save_entry(title,content)
+                return HttpResponseRedirect(reverse("entry", kwargs={"entry": title}))
+            else:
+                return render(request, "encyclopedia/newEntry.html", {
+                "form" : form,
+                "entry" : title,
+                "existing" : True
+                })
+        else:
+            return render(request, "encyclopedia/newEntry.html", {
+            "form" : form,
+            "existing" : False
+            })
+    else:
+        return render(request, "encyclopedia/newEntry.html", {
+        "form" : form,
+        "existing" :False
+        })
