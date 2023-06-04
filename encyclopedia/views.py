a5a44b112387771576eb1fe72d8724491c855221
Lucas Jacinto <lucasjacintodosantos@gmail.com>
2023-06-04 16:20:08 -0300

    implementing entry and edit

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -8,21 +8,28 @@ from django import forms
 from django.contrib.auth.decorators import login_required
 
 
+def converterMarkdownToHtml(entry):
+    contents = util.get_entry(entry)
+    markdowner = markdown.Markdown()
+    if contents == None:
+        return None
+    else:
+        return markdowner.convert(contents)
+
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
 def entry(request, entry):
-    markdowner = markdown.Markdown()
-    entryPages = util.get_entry(entry)
-    if entryPages is None:
+    contentsHtml = converterMarkdownToHtml(entry)
+    if contentsHtml == None:
         return render(request, "encyclopedia/noExistEntry.html",{
-            "entryTitle":entry
+            "entryTitle": entry
         })
     else:
         return render(request, "encyclopedia/entry.html", {
-            "entry":markdowner.convert(entryPages),
+            "entry": contentsHtml,
             "entryTitle": entry
         })
 
@@ -49,14 +56,14 @@ def random(request):
     return HttpResponseRedirect(reverse("entry", kwargs={'entry':randomEntries}))
 
 
-class newEntryForm(forms.Form):
-    title = forms.CharField(label = "Entry the Title", widget=forms.TextInput(attrs={'class' : 'form-control col-md-8 col-lg-8'}))
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Entry the Title", widget=forms.TextInput(attrs={'class' : 'form-control col-md-8 col-lg-8'}))
     content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control col-md-8 col-lg-8', 'rows': 10}))
     edit = forms.BooleanField(initial=False, widget=forms.HiddenInput(), required=False)
 
 def newEntry(request):
     if request.method == "POST":
-        form = newEntryForm(request.POST)
+        form = NewEntryForm(request.POST)
         if form.is_valid():
             title = form.cleaned_data["title"]
             content = form.cleaned_data["content"]
@@ -66,8 +73,8 @@ def newEntry(request):
             else:
                 return render(request, "encyclopedia/newEntry.html", {
                 "form" : form,
-                "entry" : title,
-                "existing" : True
+                "existing" : True,
+                "entry" : title
                 })
         else:
             return render(request, "encyclopedia/newEntry.html", {
@@ -76,7 +83,7 @@ def newEntry(request):
             })
     else:
         return render(request, "encyclopedia/newEntry.html", {
-            "form" : newEntryForm(),
+            "form" : NewEntryForm(),
             "existing" :False
         })
 
@@ -87,7 +94,7 @@ def edit(request, entry):
             "entryTitle" : entry
         })
     else:
-        form = newEntryForm()
+        form = NewEntryForm()
         form.fields["title"].initial = entry
         form.fields["title"].widget = forms.HiddenInput()
         form.fields["content"].initial = entryPages
